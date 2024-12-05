27c7062bf479820619f7a9644fa8c7c8e51a2793
Emilien <admin@emilcha.fr>
2024-12-05 21:30:29 +0100

    Cmake ad libs setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
+#include <oatpp/Environment.hpp>
 
 int main(int argc, char** argv)
 {
+    oatpp::Environment::init();
+
     std::cout << "Hello World !" << std::endl;
 
+    oatpp::Environment::destroy();
     return 0;
 }
